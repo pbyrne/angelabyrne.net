9338059ab2a2daedb049caab8dcf11e703145d16
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2025-02-20 09:28:34 -0600

    Start fleshing out the product section and request form

---
+++ source/_data/products.js
@@ -0,0 +1,27 @@
+class Product {
+  description;
+  image;
+  name;
+  slug;
+
+  constructor(data) {
+    Object.assign(this, data);
+    this.slug = this.slug || this.name.toLowerCase().replaceAll(" ", "-")
+  }
+}
+
+export default [
+
+  new Product({
+    name: "Cup sleeve",
+    image: "https://placehold.co/600x400",
+    description: "Foo bar",
+  }),
+
+  new Product({
+    name: "Coffee cup",
+    image: "https://placehold.co/600x400",
+    description: "Foo bar",
+  }),
+
+]
