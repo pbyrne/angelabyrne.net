be9df4ae28108bc1c2af439123f0d33256e0e667
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2026-07-03 14:48:49 -0500

    Add tote with multiple images

--- source/_data/products.js
+++ source/_data/products.js
@@ -1,7 +1,7 @@
 class Product {
   attribution;
   description;
-  image;
+  images;
   name;
   price;
   slug;
@@ -11,6 +11,9 @@ class Product {
     this.slug = this.slug || this.name.toLowerCase().replace(/[^a-z0-9\s-]/g, "").replace(/\s+/g, "-")
   }
 
+  get image() {
+    return this.images[0]
+  }
   get url() {
     return `/products/${this.slug}/`
   }
@@ -21,55 +24,79 @@ export default [
   new Product({
     attribution: `Pattern by <a href="https://www.myamigurumifarm.com">myamigurumifarm.com</a>`,
     description: "Approx. X″ tall by X″ wide",
-    image: "images/coffee.jpeg",
+    images: [
+      "images/coffee.jpeg",
+    ],
     name: "Coffee Cup amigurumi",
     price: 15,
   }),
 
   new Product({
     description: "Approx. X″ tall by X″ wide",
-    image: "images/placeholder.png",
-    name: "Ear warmers (adult)",
+    images: [
+      "images/placeholder.png",
+    ],
+    name: "Ear warmer (adult)",
     price: 20,
   }),
 
   new Product({
     description: "Approx. X″ tall by X″ wide",
-    image: "images/placeholder.png",
-    name: "Ear warmers (child)",
+    images: [
+      "images/placeholder.png",
+    ],
+    name: "Ear warmer (child)",
     price: 15,
   }),
 
   new Product({
     attribution: `Pattern by <a href="https://www.loopsandlovecrochet.com">www.loopsandlovecrochet.com</a>`,
     description: "Approx. X″ tall by X″ wide",
-    image: "images/heart.jpeg",
-    name: "Heart stuffies",
+    images: [
+      "images/heart.jpeg",
+    ],
+    name: "Heart stuffie",
     price: 10,
   }),
 
   new Product({
     attribution: `Original pattern inspired by <a href="https://www.etsy.com/shop/HandSpunBits">HandSpunBits on Etsy</a>`,
     description: "Approx. X″ tall by X″ wide",
-    image: "images/sleeve.jpeg",
-    name: "Cup sleeves",
+    images: [
+      "images/sleeve.jpeg",
+    ],
+    name: "Cup sleeve",
     price: 5,
   }),
 
   new Product({
     attribution: `Pattern by <a href="https://www.sweetsofties.com">sweetsofties.com</a>`,
     description: "Approx. X″ tall by X″ wide. Due to their tiny size, octo squishes may not include all colors of selected Pride yarn.",
-    image: "images/placeholder.png",
-    name: "Octo squishes",
+    images: [
+      "images/placeholder.png",
+    ],
+    name: "Octo squish",
     price: 5,
   }),
 
   new Product({
     attribution: `Pattern by <a href="https://www.youtube.com/@tallermanualperu">tallermanualperu on YouTube</a>`,
     description: "Approx. X″ tall by X″ wide",
-    image: "images/placeholder.png",
-    name: "Roses",
+    images: [
+      "images/placeholder.png",
+    ],
+    name: "Rose",
     price: 15,
+  }),
+
+  new Product({
+    description: "TK description for tote bag",
+    images: [
+      "images/tote-pride.jpeg",
+      "images/tote-trans.jpeg",
+    ],
+    name: "Tote bag",
+    price: 100,
   }),
 
 ]
